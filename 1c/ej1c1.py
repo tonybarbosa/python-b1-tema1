0ddcea3bb0b9cdfece20b2c6edb194c9222107f6
MarcosTardy <marcostardsan@gmail.com>
2025-03-14 12:52:11 +0100

    ej1c1 tema 1

--- 1c/ej1c1.py
+++ 1c/ej1c1.py
@@ -46,8 +46,11 @@ Exemple:
 
 
 def mult_recursive(value, times):
-    # Write here your code
-    pass
+    if times == 1:
+        return value
+    else:
+        return value + mult_recursive(value, times-1)
+print(mult_recursive(2,10))
 
 
 # Si quieres probar tu código, descomenta las siguientes líneas y ejecuta el script
