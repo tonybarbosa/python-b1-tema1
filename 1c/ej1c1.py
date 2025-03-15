ab360606f9984e9a4ea791087e4c756230e53604
mponce1uoc <122378581+mponce1uoc@users.noreply.github.com>
2025-03-15 00:11:01 -0500

    Update ej1c1.py

--- 1c/ej1c1.py
+++ 1c/ej1c1.py
@@ -46,10 +46,8 @@ Exemple:
 
 
 def mult_recursive(value, times):
-    if times == 1:
-        return value
-    else:
-        return value + mult_recursive(value, times - 1)
+    # Write here your code
+    pass
 
 
 # Si quieres probar tu código, descomenta las siguientes líneas y ejecuta el script
